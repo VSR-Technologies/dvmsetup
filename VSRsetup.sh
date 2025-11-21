6338eceef9554fa503c22d711ee5c6ba7cb7756d
ctpenguin11 <ctpenguin11@gmail.com>
2025-11-21 09:54:06 -0700

    Add VSRsetup.sh for automated installation
    This script automates the installation of DVM host and systemd services for VSR Technologies.

---
+++ VSRsetup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# VSR TECHNOLOGIES
+# DO NOT MODIFY ANYTHING IN THIS FILE
+sudo apt install cmake
+sudo apt update
+git clone https://github.com/DVMProject/dvmhost.git
+cd dvmhost
+mkdir build
+cd build
+cmake ..
+make
+make strip
+make tarball
+sudo tar xvzf dvmhost_R04Gxx_<arch>.tar.gz -C /opt
+sudo make old_install
+cd
+sudo rm -r dvmhost
+git clone https://github.com/VSR-Technologies/DVMsystemd.git
+cd DVMsystemd
+cd systemctl
+sudo mv ./cc.service /usr/bin/systemctl
+sudo mv ./vc.service /usr/bin/systemctl
+cd
+sudo systemctl enable cc.service
+sudo systemctl enable vc.service
+echo INSTALL SUCCESS
+sudo reboot
