46f41114703958411ca0b132adcc9261e8b98515
ctpenguin11 <ctpenguin11@gmail.com>
2025-11-21 09:12:22 -0700

    Update VSRBOOT2.sh with legacy systemd note
    Added note about legacy systemd files in the script.

--- bootscripts/VSRBOOT2.sh
+++ bootscripts/VSRBOOT2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This is for trunking sites
+# These are legacy and the new systemd files should be used instead
 sudo /opt/dvm/bin/dvmhost -c /opt/dvm/bin/VC.yml
 sudo /opt/dvm/bin/dvmhost -c /opt/dvm/bin/CC.yml -f
