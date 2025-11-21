375814e6615ab4761f3b5d812a9448768c31685f
ctpenguin11 <ctpenguin11@gmail.com>
2025-11-21 09:12:08 -0700

    Replace legacy command with systemd usage
    Updated VSRBOOT.sh to use the new systemd files instead of legacy commands.

--- bootscripts/VSRBOOT.sh
+++ bootscripts/VSRBOOT.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This is for Conventional, DVRS, and VoC Trunking Sites
+# These are legacy and the new systemd files should be used instead
 sudo /opt/dvm/bin/dvmhost -c /opt/dvm/bin/config.yml -f
